e0df9753e9e3e8ec1a9d14eafe2359a3c32919d7
hazem samir <hazem.s.ashmawy@gmail.com>
2016-09-04 09:57:28 +0300

    cpp template

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* macros */
+// memory
+#define zeroset(A) memset((A), 0, sizeof((A)))
+#define negset(A) memset((A), -1, sizeof((A)))
+#define all(c) (c).begin(), (c).end()
+#define pb push_back
+// input & output
+#define inpt(a) freopen(a,"r",stdin)
+#define otpt(a) freopen(a,"w",stdout)
+#define eol "\n"
+// types
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll,ll> pll;
+#define ff first
+#define ss second
+// constants
+#define EPS 1e-9
+#define MAXN 100005
+#define INF INT_MAX
+#define MOD 1000000007
+
+int main()
+{
+    //ios::sync_with_stdio(false);
+    return 0;
+}
